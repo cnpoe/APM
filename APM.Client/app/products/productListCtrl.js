018e96d274ea585c6b945fe2b8902888f219a375
cnpoe <cnpoek@gmail.com>
2017-09-03 02:57:30 -0500

    First controller and view

---
+++ APM.Client/app/products/productListCtrl.js
@@ -0,0 +1,54 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("productManagement")
+        .controller("ProductListCtrl", ProductListCtrl);
+
+    function ProductListCtrl() {
+        var vm = this;
+
+        vm.products = [
+            {
+                "productId": 1,
+                "productName": "Leaft Rake",
+                "productCode": "GDN-0011",
+                "ReleaseDate": "March 19, 2009",
+                "description": "lorem ipsum dolor sit amet consectetur adipiscing elit",
+                "price": 19.95
+            },
+            {
+                "productId": 2,
+                "productName": "Leaft Rake",
+                "productCode": "GDN-0011",
+                "ReleaseDate": "March 19, 2009",
+                "description": "lorem ipsum dolor sit amet consectetur adipiscing elit",
+                "price": 19.95
+            },
+            {
+                "productId": 3,
+                "productName": "Leaft Rake",
+                "productCode": "GDN-0011",
+                "ReleaseDate": "March 19, 2009",
+                "description": "lorem ipsum dolor sit amet consectetur adipiscing elit",
+                "price": 19.95
+            },
+            {
+                "productId": 4,
+                "productName": "Leaft Rake",
+                "productCode": "GDN-0011",
+                "ReleaseDate": "March 19, 2009",
+                "description": "lorem ipsum dolor sit amet consectetur adipiscing elit",
+                "price": 19.95
+            },
+            {
+                "productId": 5,
+                "productName": "Leaft Rake",
+                "productCode": "GDN-0011",
+                "ReleaseDate": "March 19, 2009",
+                "description": "lorem ipsum dolor sit amet consectetur adipiscing elit",
+                "price": 19.95
+            },
+        ]
+    }
+}());
--- APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM.WebAPI/App_Start/WebApiConfig.cs
@@ -25,6 +25,8 @@ namespace APM.WebAPI
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
         }
     }
 }
