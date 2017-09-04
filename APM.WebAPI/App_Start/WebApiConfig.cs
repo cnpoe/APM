c473ce97a7579becd9a8c349bc36470ac2a20a83
cnpoe <cnpoek@gmail.com>
2017-09-03 23:58:19 -0500

    Controller and repository for products

--- APM.WebAPI/App_Start/WebApiConfig.cs
+++ APM.WebAPI/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Net.Http.Headers;
 
 namespace APM.WebAPI
 {
@@ -24,9 +25,9 @@ namespace APM.WebAPI
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
-            );
+            ); 
 
-            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("text/html"));
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
