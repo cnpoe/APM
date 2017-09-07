608986f8ced8e6d0edb98789f874102af24168ad
cnpoe <cnpoek@gmail.com>
2017-09-06 23:34:07 -0500

    Se añade CORS

--- APM.WebAPI/Controllers/ProductsController.cs
+++ APM.WebAPI/Controllers/ProductsController.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace APM.WebAPI.Controllers
 {
+    [EnableCorsAttribute("http://localhost:57101", "*","*")]
     public class ProductsController : ApiController
     {
         // GET: api/Products
